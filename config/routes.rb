c47917ca33bfc666dd3cdefc20da2fc2c9840796
Nathan Beck <nathan.beck10@gmail.com>
2014-11-04 14:39:23 -0500

    Admins can create areas

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resource :session, only: [:new, :create, :destroy]
   resources :users, only: [:new, :create]
-  resources :areas, only: [:create]
+  resources :areas, only: [:create, :index]
 
   resources :posts, only: [:index, :create, :update, :edit, :destroy]
   resources :categories, only: [:create]
