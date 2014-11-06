4c45caaff0fc8e2b12f882deec380887fc2172ed
Nathan Beck <nathan.beck10@gmail.com>
2014-11-06 16:40:31 -0500

    Show current users posts by area

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ActiveRecord::Base
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
+
+  def posts_for_area(area)
+    area.posts.where(
+      "user_id = :user_id and area_id = :area_id",
+      user_id: self.id, area_id: area.id
+    )
+  end
 end
